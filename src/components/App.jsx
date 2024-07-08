a8e13b7ee8cc71bad399cbb80701be56aa855a40
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-07-08 23:26:52 +0200

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-export const App = () => {
+/*export const App = () => {
   return (
     <div
       style={{
@@ -14,3 +14,35 @@ export const App = () => {
     </div>
   );
 };
+*/
+import React from 'react';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import AboutUs from './components/AboutUs';
+import Products from './components/Products';
+import Shop from './components/Shop';
+import Gallery from './components/Gallery';
+import Beauty from './components/Beauty';
+import Reviews from './components/Reviews';
+import MeetUs from './components/MeetUs';
+import Footer from './components/Footer';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <AboutUs />
+      <Products />
+      <Shop />
+      <Gallery />
+      <Beauty />
+      <Reviews />
+      <MeetUs />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/MeetUs/MeetUs.jsx
@@ -0,0 +1,11 @@
+const MeetUs = () => {
+    return (
+      <section id="meet-us">
+        <h2>Meet Us</h2>
+        <p>Text about the team or company.</p>
+        <img src="path-to-image" alt="Meet Us" />
+      </section>
+    );
+  };
+  
+  export default MeetUs;
