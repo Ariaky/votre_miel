672df3e9116cc3fc49f13e168255468ff222d26c
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-07-08 23:22:45 +0200

    gallery

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,14 @@
+const Gallery = () => {
+    return (
+      <section id="gallery">
+        <h2>Gallery</h2>
+        <div className="gallery-images">
+          <img src="path-to-image" alt="Gallery Image 1" />
+          <img src="path-to-image" alt="Gallery Image 2" />
+          <img src="path-to-image" alt="Gallery Image 3" />
+        </div>
+      </section>
+    );
+  };
+  
+  export default Gallery;
