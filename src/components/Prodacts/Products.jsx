b582e5982bb62012877e2624d54e01f02a16164f
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-07-08 22:54:45 +0200

    products

---
+++ src/components/Prodacts/Products.jsx
@@ -0,0 +1,15 @@
+const Products = () => {
+    return (
+      <section id="products">
+        <h2>Products</h2>
+        <div className="product-icons">
+          <div className="product-icon">Product 1</div>
+          <div className="product-icon">Product 2</div>
+          <div className="product-icon">Product 3</div>
+          <div className="product-icon">Product 4</div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Products;
