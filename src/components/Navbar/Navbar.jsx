6c76b1bc27564706d88a2734301da72888c91884
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-07-08 22:50:37 +0200

    navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+      <nav>
+        <div className="logo">Golden Hive Honey</div>
+        <ul>
+          <li><a href="#about-us">About Us</a></li>
+          <li><a href="#products">Products</a></li>
+          <li><a href="#shop">Shop</a></li>
+          <li><a href="#gallery">Gallery</a></li>
+          <li><a href="#beauty">Beauty</a></li>
+          <li><a href="#reviews">Reviews</a></li>
+          <li><a href="#meet-us">Meet Us</a></li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
