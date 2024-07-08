8d56cf5e1e41d591f40e2b3901340c650cbaf655
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-07-08 22:53:42 +0200

    about us

---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,11 @@
+const AboutUs = () => {
+    return (
+      <section id="about-us">
+        <h2>About Us</h2>
+        <p>Your text here about the company.</p>
+        <img src="path-to-image" alt="About Us" />
+      </section>
+    );
+  };
+  
+  export default AboutUs;
