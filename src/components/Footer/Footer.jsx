d8053feb31ff7c9f51a2a4cb85784d7c018cb8cd
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-07-08 23:18:42 +0200

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+      <footer>
+        <section id="subscribe">
+          <h2>Subscribe to Our Newsletter</h2>
+          <form>
+            <input type="email" placeholder="Enter your email" />
+            <button type="submit">Subscribe</button>
+          </form>
+        </section>
+        <p>&copy; 2024 Golden Hive Honey. All rights reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
