1cbdd8007e6b303edb2cc19d8907d1d0352fc5d5
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-07-08 23:23:41 +0200

    beauty

---
+++ src/components/Beauty/Beauty.jsx
@@ -0,0 +1,11 @@
+const Beauty = () => {
+    return (
+      <section id="beauty">
+        <h2>Beauty</h2>
+        <p>Text about beauty products.</p>
+        <img src="path-to-image" alt="Beauty" />
+      </section>
+    );
+  };
+  
+  export default Beauty;
