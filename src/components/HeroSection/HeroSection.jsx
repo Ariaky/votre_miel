35854c671ae9fab78638d7a8c2370f5b27165127
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-07-08 22:51:42 +0200

    hero

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,10 @@
+const HeroSection = () => {
+    return (
+      <section className="hero">
+        <h1>Golden Hive Honey</h1>
+        <p>Discover our finest selection of honey and beeswax products.</p>
+      </section>
+    );
+  };
+  
+  export default HeroSection;
