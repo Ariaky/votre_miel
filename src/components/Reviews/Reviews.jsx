c4b107819458d7c01a106414cb2e5a214f8d0a4f
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-07-08 23:24:30 +0200

    reviews

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,19 @@
+const Reviews = () => {
+    return (
+      <section id="reviews">
+        <h2>Reviews</h2>
+        <div className="review-cards">
+          <div className="review-card">
+            <p>Review text</p>
+            <p>Reviewer name</p>
+          </div>
+          <div className="review-card">
+            <p>Review text</p>
+            <p>Reviewer name</p>
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Reviews;
