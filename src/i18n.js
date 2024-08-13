a7a1528b205fe51294d54ffc10082348785ad5e4
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-08-13 16:51:24 +0200

    translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,3 +1,18 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import translationEn from './data/translationEn.json';
+import translationFr from './data/translationFr.json';
+import translationUa from './data/translationUa.json';
+
+const resources = {
+    fr: {
+        translation: translationFr
+    },
+    en: {
+        translation: translationEn
+    },
+    ua: {
+        translation:translationUa
+    }
+};
+
