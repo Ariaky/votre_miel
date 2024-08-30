26b0111e84a505d440393422ce58d67863d29d48
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-08-30 23:27:33 +0200

    button translation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,15 +4,15 @@ const Navbar = () => {
   const { t } = useTranslation();
     return (
       <nav>
-        <div className="logo">Votre miel</div>
+        <div className="logo">{t('navbar.home')}</div>
         <ul>
-          <li><a href="#about-us">About Us</a></li>
-          <li><a href="#products">Products</a></li>
-          <li><a href="#shop">Shop</a></li>
-          <li><a href="#gallery">Gallery</a></li>
-          <li><a href="#beauty">Beauty</a></li>
-          <li><a href="#reviews">Reviews</a></li>
-          <li><a href="#meet-us">Meet Us</a></li>
+          <li><a href="#about-us">{t('navbar.about')}</a></li>
+          <li><a href="#products">{t('navbar.products')}</a></li>
+          <li><a href="#shop">{t('navbar.shop')}</a></li>
+          <li><a href="#gallery">{t('navbar.gallery')}</a></li>
+          <li><a href="#beauty">{t('navbar.beauty')}</a></li>
+          <li><a href="#reviews">{t('navbar.reviews')}</a></li>
+          <li><a href="#meet-us">{t('navbar.meet_us')}</a></li>
         </ul>
       </nav>
     );
