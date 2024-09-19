a84442f2ed23fe191a99e3e8c21cdcef789b918f
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-09-19 22:43:19 +0200

    translation

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,10 @@
+
 const Products = () => {
     return (
       <section id="products">
         <h2>Products</h2>
         <div className="product-icons">
-          <div className="product-icon">Product 1</div>
+          <div className="product-icon">{t('product.product')}</div>
           <div className="product-icon">Product 2</div>
           <div className="product-icon">Product 3</div>
           <div className="product-icon">Product 4</div>
