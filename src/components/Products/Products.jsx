3a7d880bcda5e57af4d97bf1a374437da0870340
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-09-20 12:30:05 +0200

    translation

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,8 +1,10 @@
+import { useTranslation } from "react-i18next";
 
 const Products = () => {
+    const {t} = useTranslation();
     return (
       <section id="products">
-        <h2>Products</h2>
+        <h2>{t('product.title')}</h2>
         <div className="product-icons">
           <div className="product-icon">{t('product.product')}</div>
           <div className="product-icon">Product 2</div>
