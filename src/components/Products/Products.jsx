f7a12db450bf5b1b2d1de15d7b53894d579d79be
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-09-29 09:14:07 +0200

    commit

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -6,9 +6,9 @@ const Products = () => {
       <section id="products">
         <h2>{t('product.title')}</h2>
         <div className="product-icons">
-          <div className="product-icon">{t('product.product')}</div>
-          <div className="product-icon">Product 2</div>
-          <div className="product-icon">Product 3</div>
+          <div className="product-icon">{t('product.product_1')}</div>
+          <div className="product-icon">{t('product.product_2')}</div>
+          <div className="product-icon">{t('product.product_3')}</div>
           <div className="product-icon">Product 4</div>
         </div>
       </section>
