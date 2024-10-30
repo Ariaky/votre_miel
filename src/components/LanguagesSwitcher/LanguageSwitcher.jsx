9fac8c0f994e6f4110028d53eb0e92ce3b71f65b
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-10-30 16:37:23 +0100

    img

--- src/components/LanguagesSwitcher/LanguageSwitcher.jsx
+++ src/components/LanguagesSwitcher/LanguageSwitcher.jsx
@@ -15,7 +15,7 @@ const LanguageSwitcher = () => {
       <button className={css.language} conClick={() => changeLanguage('en')}>English</button>
       <button onClick={() => changeLanguage('fr')}>Français</button>
       <button onClick={() => changeLanguage('ua')}>Українська</button>
-      <img src={LanguageSwitcherImg} alt="img" width="30" height="20" />
+      <img src={LanguageSwitcherImg} alt="img" width="128" height="64" />
       <img src={LanguageSwitcherClickOnImg} alt="imgClickOn" width="30" height="20" />
     </div>
   );
