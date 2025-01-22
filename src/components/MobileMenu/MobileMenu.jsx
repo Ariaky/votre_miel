949dc8c48a399dba40c91a7e886ba3e543b2eced
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2025-01-22 23:08:49 +0100

    commit

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -3,6 +3,26 @@ import { useTranslation } from 'react-i18next';
 const MobileMenu = () => {
     const  { t } = useTranslation();
     return (
-        
+        <button
+            className="header-menu-toggle js-open-menu"
+            aria-expanded="false"
+            aria-controls="mobile-menu"
+            type="button"
+          >
+            <svg
+              className="header-menu-icon header-menu-icon-mobile"
+              width="28"
+              height="28"
+            >
+              <use href="/image/sprite.svg#icon-menu-28"></use>
+            </svg>
+            <svg
+              className="header-menu-icon header-menu-icon-tablet"
+              width="32"
+              height="32"
+            >
+              <use href="/image/sprite.svg#icon-menu-32"></use>
+            </svg>
+        </button>
     );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,27 +20,7 @@ const Navbar = () => {
         <LanguageSwitcher />  
         </button>
         
-        <button
-            className="header-menu-toggle js-open-menu"
-            aria-expanded="false"
-            aria-controls="mobile-menu"
-            type="button"
-          >
-            <svg
-              className="header-menu-icon header-menu-icon-mobile"
-              width="28"
-              height="28"
-            >
-              <use href="/image/sprite.svg#icon-menu-28"></use>
-            </svg>
-            <svg
-              className="header-menu-icon header-menu-icon-tablet"
-              width="32"
-              height="32"
-            >
-              <use href="/image/sprite.svg#icon-menu-32"></use>
-            </svg>
-        </button>
+        
         
       </nav>
     );
