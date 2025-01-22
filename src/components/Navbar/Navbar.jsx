c60488c7e0c441a7e4ffec9a6fe974165c872256
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2025-01-22 23:11:06 +0100

    commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import LanguageSwitcher from "../LanguagesSwitcher/LanguageSwitcher";
+import MobileMenu from "components/MobileMenu/MobileMenu";
 import css from "./Navbar.module.css";
 
 const Navbar = () => {
